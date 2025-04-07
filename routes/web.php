e353425262c9a5a4c7376b2361949aa82fcecdbe
Belladona2440 <belladonainblue@gmail.com>
2025-04-07 12:23:22 +0800

    change url, add route name

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/login', [UserController::class, 'login'])->name('login');
-Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::get('/register', [RegisterController::class, 'create'])->name('register.create');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+Route::get('/login', [SessionController::class, 'create'])->name('login.create');
+Route::post('/login', [SessionController::class, 'store'])->name('login.store');
+Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
 
 Route::get('/profile', [UserController::class, 'profile'])->name('profile');
