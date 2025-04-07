eeb502de652a933577e7bb5454fd2a8a68147a9f
Belladona2440 <belladonainblue@gmail.com>
2025-04-07 12:05:10 +0800

    changed route for login and register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,39 +6,28 @@
       <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
         <div class="mb-5">
           <div class="text-center mb-4">
-            <a href="#">
+            <a href="{{ route('login.create') }}">
               <img src="{{ asset('storage/logo.png') }}" alt="logo" width="175" height="57">
             </a>
           </div>
         </div>
         <div class="card border border-dark rounded-4">
           <div class="card-body p-3 p-md-4 p-xl-5">
-            <form action="#!">
+            <form action="{{ route('login.store') }}" method="POST">
+              @csrf
               <div class="row gy-3 overflow-hidden">
                 <h4 class="text-center mb-4">Mag sign in ka na</h4>
-                <div class="col-12">
-                  <div class="form-floating border border-dark rounded mb-3">
-                    <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
-                    <label for="email">Email</label>
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="form-floating border border-dark rounded mb-3">
-                    <input type="password" class="form-control" name="password" id="password" value="" placeholder="Password" required>
-                    <label for="password">Password</label>
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" name="remember_me" id="remember_me">
-                    <label class="form-check-label text-secondary" for="remember_me">
-                      Keep me logged in
-                    </label>
-                  </div>
-                </div>
+                <x-form-field>
+                  <x-form-floating type="email" name="email" id="email" placeholder="choi@kupal.com" label="Email" value="{{ old('email') }}" />
+                  <x-form-error name="email" />
+                </x-form-field>
+                <x-form-field>
+                  <x-form-floating type="password" name="password" id="password" placeholder="Password" label="Password" />
+                </x-form-field>
+                <x-form-checkbox name="remember_me" id="remember_me" for="remember_me" label="Keep me signed in" />
                 <div class="col-12">
                   <div class="d-grid">
-                    <button class="btn btn-primary btn-lg" type="submit">Log in</button>
+                    <x-form-button>Sign in</x-form-button>
                   </div>
                 </div>
               </div>
@@ -46,7 +35,7 @@
           </div>
         </div>
         <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-center mt-4">
-          <a href="{{ route('register') }}" class="link-secondary text-decoration-none">Create new account</a>
+          <a href="{{ route('register.create') }}" class="link-secondary text-decoration-none">Create new account</a>
           <a href="#!" class="link-secondary text-decoration-none">Forgot password</a>
         </div>
       </div>
