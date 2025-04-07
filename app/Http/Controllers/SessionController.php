adee52b86a309ae4b7762d812a66a518d7a40224
Belladona2440 <belladonainblue@gmail.com>
2025-04-07 12:27:51 +0800

    add session controller

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\user;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+
+use function PHPUnit\Framework\throwException;
+
+class SessionController extends Controller
+{
+  public function create() {
+    return view('auth.login');
+  }
+
+  public function store() {
+    //validate
+    $validate = request()->validate([
+      'email' => ['required', 'email'],
+      'password' => ['required']
+    ]);
+    //attempt to login
+    if (! Auth::attempt($validate)) {
+      throw ValidationException::withMessages([
+        'email' => 'Sorry, these credentials do not match'
+      ]);
+    };
+    //regenarate token
+    request()->session()->regenerate();
+    //redirect
+    return redirect('/');
+  }
+  public function destroy() {
+    Auth::logout();
+
+    return redirect('/');
+  }
+}
