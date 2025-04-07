be5d09b459e9f7d457f708511b77c06d508958d5
Belladona2440 <belladonainblue@gmail.com>
2025-04-07 12:34:37 +0800

    add component form-floating

---
+++ resources/views/components/form-floating.blade.php
@@ -0,0 +1,6 @@
+@props(['type' => 'text', 'name', 'id' => $name, 'placeholder' => '', 'value', 'label'])
+
+<div class="form-floating border border-dark rounded mb-3">
+  <input type="{{ $type }}" class="form-control @error('{{ $name }}') is-invalid @enderror" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}" value="{{ old($name) }}" required>
+  <label for="{{ $id }}">{{ $label }}</label>
+</div>
