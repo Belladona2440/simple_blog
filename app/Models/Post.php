45fb6fd745c1d7ab8a8d0d2860a48b657bc9d5dc
Belladona2440 <belladonainblue@gmail.com>
2025-04-12 15:52:36 +0800

    add protected fillable for posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+  protected $fillable = [
+    'post_category',
+    'post_title',
+    'post_content',
+    'user_id'
+  ];
 }
