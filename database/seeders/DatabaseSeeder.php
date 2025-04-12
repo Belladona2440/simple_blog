20b4993d923627139ea62dc802bbaf672bfbad1b
Belladona2440 <belladonainblue@gmail.com>
2025-04-12 16:11:03 +0800

    comment out post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-        /Post::factory(5)->create();
+        //Post::factory(5)->create();
 
         /* User::factory()->create([
             'name' => 'Test User',
