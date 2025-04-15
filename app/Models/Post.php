00988209a48bdd98702dcc5f72d90a9dab0ca80e
Belladona2440 <belladonainblue@gmail.com>
2025-04-15 14:40:11 +0800

    mass assign bg_img

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
   protected $fillable = [
-    'post_category',
-    'post_title',
-    'post_content',
-    'user_id'
+    'category',
+    'title',
+    'content',
+    'user_id',
+    'author',
+    'bg_img'
   ];
 }
