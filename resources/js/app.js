b8fe2db7905386933a63c76f43ceab8b51580fe9
Belladona2440 <belladonainblue@gmail.com>
2025-04-15 14:50:57 +0800

    import Swal2

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,37 @@ import './bootstrap';
 
 import '../scss/app.scss';
 import * as bootstrap from 'bootstrap';
+import Swal from 'sweetalert2';
+//window.Swal = Swal; //use globally
+
+window.confirmLogout = function () {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You will be logged out.",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, logout',
+    cancelButtonText: 'Cancel'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.getElementById('logout-form').submit();
+    }
+  });
+}
+
+  function previewImage(event) {
+    const input = event.target;
+    const preview = document.getElementById('imagePreview');
+
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+        preview.style.display = 'block';
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
