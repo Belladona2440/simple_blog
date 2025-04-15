bb6df127bd7e03fce60d78ef2176aa77bcbac056
Belladona2440 <belladonainblue@gmail.com>
2025-04-15 15:01:53 +0800

    add routes for showing post and fixed route home

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,12 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+// Route::get('/', function () {
+//     return view('home', ['posts' => $posts]);
+// })->name('home');
 
 Route::get('/register', [RegisterController::class, 'create'])->name('register.create');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
@@ -19,5 +20,7 @@ Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
 
 Route::get('/profile', [UserController::class, 'profile'])->name('profile');
 
+Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
 Route::post('/posts/create', [PostController::class, 'store'])->name('post.store');
+Route::get('/posts/show', [PostController::class, 'show'])->name('post.show');
