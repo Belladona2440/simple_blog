478d51d774bbe28f1f9b8b38761a45d2f052ad73
Belladona2440 <belladonainblue@gmail.com>
2025-04-19 21:39:41 +0800

    added ckeditor 5 cdn and script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>  --}}
     <title>Document</title>
+    @if (session('logoutSuccess'))
+      <script>
+        window.logoutSuccess = "{{ session('logoutSuccess') }}";
+      </script>
+    @endif
     @vite('resources/js/app.js')
   </head>
   <body>
     <x-nav></x-nav>
       @yield('content')
     <x-footer></x-footer>
+    @yield('scripts')
+{{--     <script> 
+      ClassicEditor
+        .create(document.querySelector('#content'), 
+        {
+          ckfinder: {
+            uploadUrl:"{{ route('ckeditor.upload', ['_token' => csrf_token()]) }}"
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    </script> --}}  
   </body>
 </html>
