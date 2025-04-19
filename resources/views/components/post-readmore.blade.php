5a3d0441c4aca9486ce1ba47b23e652af98e6145
Belladona2440 <belladonainblue@gmail.com>
2025-04-19 21:23:35 +0800

    add route for showing single post

--- resources/views/components/post-readmore.blade.php
+++ resources/views/components/post-readmore.blade.php
@@ -1,6 +1,4 @@
-
-
-<a href="{{ route('post.show') }}" class="readmore">
+<a href="{{ route('post.show', $post->id) }}" class="readmore">
   <span>Read More</span>
   <i class="bi bi-arrow-right"></i>
 </a>
