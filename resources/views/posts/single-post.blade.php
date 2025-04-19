1b848853ae1f5a3346d0a2f352cba3028204913d
Belladona2440 <belladonainblue@gmail.com>
2025-04-19 21:43:43 +0800

    edited article section

--- resources/views/posts/single-post.blade.php
+++ resources/views/posts/single-post.blade.php
@@ -1,76 +1,37 @@
-
-            <section id="blog-details" class="blog-details section">
-  
-                <article class="article">
-  
-                  <div class="post-img">
-                    <img src="assets/img/blog/blog-1.jpg" alt="" class="img-fluid">
-                  </div>
-  
-                  <h2 class="title">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</h2>
-  
-                  <div class="meta-top">
-                    <ul>
-                      <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-details.html">John Doe</a></li>
-                      <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2020-01-01">Jan 1, 2022</time></a></li>
-                      <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="blog-details.html">12 Comments</a></li>
-                    </ul>
-                  </div><!-- End meta top -->
-  
-                  <div class="content">
-                    <p>
-                      Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-                      Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
-                    </p>
-  
-                    <p>
-                      Sit repellat hic cupiditate hic ut nemo. Quis nihil sunt non reiciendis. Sequi in accusamus harum vel aspernatur. Excepturi numquam nihil cumque odio. Et voluptate cupiditate.
-                    </p>
-  
-                    <blockquote>
-                      <p>
-                        Et vero doloremque tempore voluptatem ratione vel aut. Deleniti sunt animi aut. Aut eos aliquam doloribus minus autem quos.
-                      </p>
-                    </blockquote>
-  
-                    <p>
-                      Sed quo laboriosam qui architecto. Occaecati repellendus omnis dicta inventore tempore provident voluptas mollitia aliquid. Id repellendus quia. Asperiores nihil magni dicta est suscipit perspiciatis. Voluptate ex rerum assumenda dolores nihil quaerat.
-                      Dolor porro tempora et quibusdam voluptas. Beatae aut at ad qui tempore corrupti velit quisquam rerum. Omnis dolorum exercitationem harum qui qui blanditiis neque.
-                      Iusto autem itaque. Repudiandae hic quae aspernatur ea neque qui. Architecto voluptatem magni. Vel magnam quod et tempora deleniti error rerum nihil tempora.
-                    </p>
-  
-                    <h3>Et quae iure vel ut odit alias.</h3>
-                    <p>
-                      Officiis animi maxime nulla quo et harum eum quis a. Sit hic in qui quos fugit ut rerum atque. Optio provident dolores atque voluptatem rem excepturi molestiae qui. Voluptatem laborum omnis ullam quibusdam perspiciatis nulla nostrum. Voluptatum est libero eum nesciunt aliquid qui.
-                      Quia et suscipit non sequi. Maxime sed odit. Beatae nesciunt nesciunt accusamus quia aut ratione aspernatur dolor. Sint harum eveniet dicta exercitationem minima. Exercitationem omnis asperiores natus aperiam dolor consequatur id ex sed. Quibusdam rerum dolores sint consequatur quidem ea.
-                      Beatae minima sunt libero soluta sapiente in rem assumenda. Et qui odit voluptatem. Cum quibusdam voluptatem voluptatem accusamus mollitia aut atque aut.
-                    </p>
-                    <img src="assets/img/blog/blog-inside-post.jpg" class="img-fluid" alt="">
-  
-                    <h3>Ut repellat blanditiis est dolore sunt dolorum quae.</h3>
-                    <p>
-                      Rerum ea est assumenda pariatur quasi et quam. Facilis nam porro amet nostrum. In assumenda quia quae a id praesentium. Quos deleniti libero sed occaecati aut porro autem. Consectetur sed excepturi sint non placeat quia repellat incidunt labore. Autem facilis hic dolorum dolores vel.
-                      Consectetur quasi id et optio praesentium aut asperiores eaque aut. Explicabo omnis quibusdam esse. Ex libero illum iusto totam et ut aut blanditiis. Veritatis numquam ut illum ut a quam vitae.
-                    </p>
-                    <p>
-                      Alias quia non aliquid. Eos et ea velit. Voluptatem maxime enim omnis ipsa voluptas incidunt. Nulla sit eaque mollitia nisi asperiores est veniam.
-                    </p>
-  
-                  </div><!-- End post content -->
-  
-                  <div class="meta-bottom">
-                    <i class="bi bi-folder"></i>
-                    <ul class="cats">
-                      <li><a href="#">Business</a></li>
-                    </ul>
-  
-                    <i class="bi bi-tags"></i>
-                    <ul class="tags">
-                      <li><a href="#">Creative</a></li>
-                      <li><a href="#">Tips</a></li>
-                      <li><a href="#">Marketing</a></li>
-                    </ul>
-                  </div><!-- End meta bottom -->
-  
-                </article>
-            </section><!-- /Blog Details Section -->
+@extends('layouts.app')
+@section('content')
+<header class="masthead" style="background-image: url('{{ asset('storage/'.$post->bg_img) }}')">
+  <div class="container position-relative px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+          <div class="col-md-10 col-lg-8 col-xl-7">
+              <div class="post-heading">
+                  <h1>{{ $post->title }}</h1>
+                  <h2 class="subheading"></h2>
+                  <span class="meta">
+                      Posted by
+                      <a href="#!">{{ $post->author }}</a>
+                      on {{ $post->created_at->format('F j, Y') }}
+                  </span>
+              </div>
+          </div>
+      </div>
+  </div>
+</header>
+<article class="mb-2">
+  <div class="container px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+          <div class="col-md-10 col-lg-8 col-xl-7">
+              <p>{{$post->content}}</p>
+              <h2 class="section-heading"></h2>
+              {{-- <a href="#!"><img class="img-fluid" src="assets/img/post-sample-image.jpg" alt="..." /></a> --}}
+              <span class="caption text-muted"></span>
+              {{-- <p>
+                  <a href="http://spaceipsum.com/"></a>
+                  &middot; Images by
+                  <a href="https://www.flickr.com/photos/nasacommons/"></a>
+              </p> --}}
+          </div>
+      </div>
+  </div>
+</article>
+@endsection
