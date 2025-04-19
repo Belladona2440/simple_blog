e19b9b5e7a48ecde3513c966f4eea4b32e179ece
Belladona2440 <belladonainblue@gmail.com>
2025-04-19 21:45:02 +0800

    add routes for upload image and implement middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CKEditorController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RegisterController;
@@ -7,20 +8,19 @@ use App\Http\Controllers\SessionController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('home', ['posts' => $posts]);
-// })->name('home');
-
 Route::get('/register', [RegisterController::class, 'create'])->name('register.create');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
-Route::get('/login', [SessionController::class, 'create'])->name('login.create');
+Route::get('/login', [SessionController::class, 'login'])->name('login')->middleware('guest');
 Route::post('/login', [SessionController::class, 'store'])->name('login.store');
 Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
 
 Route::get('/profile', [UserController::class, 'profile'])->name('profile');
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+Route::get('/my-posts', [PostController::class, 'display'])->name('my-posts.display');
+Route::get('/posts/create', [PostController::class, 'create'])->name('post.create')->middleware('auth');
 Route::post('/posts/create', [PostController::class, 'store'])->name('post.store');
-Route::get('/posts/show', [PostController::class, 'show'])->name('post.show');
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('post.show')->middleware('auth');
+
+Route::post('/ckeditor/upload', [CKEditorController::class, 'upload'])->name('ckeditor.upload');
