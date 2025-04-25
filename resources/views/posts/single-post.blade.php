bff73d0f3c209ea1f470763a5fd0e21f499a351c
Belladona2440 <belladonainblue@gmail.com>
2025-04-25 23:05:20 +0800

    add back to previous page link

--- resources/views/posts/single-post.blade.php
+++ resources/views/posts/single-post.blade.php
@@ -19,19 +19,23 @@
 </header>
 <article class="mb-2">
   <div class="container px-4 px-lg-5">
-      <div class="row gx-4 gx-lg-5 justify-content-center">
-          <div class="col-md-10 col-lg-8 col-xl-7">
-              <p>{{$post->content}}</p>
-              <h2 class="section-heading"></h2>
-              {{-- <a href="#!"><img class="img-fluid" src="assets/img/post-sample-image.jpg" alt="..." /></a> --}}
-              <span class="caption text-muted"></span>
-              {{-- <p>
-                  <a href="http://spaceipsum.com/"></a>
-                  &middot; Images by
-                  <a href="https://www.flickr.com/photos/nasacommons/"></a>
-              </p> --}}
-          </div>
+    <div class="row">
+      <div class="col-6 justify-content-start mb-4">
+        <a href="{{ url()->previous() }}" class="btn btn-secondary px-">Back to posts</a>
       </div>
+      {{-- @auth
+      <div class="col-6 justify-content-end d-flex gap-4 mb-4">
+        <a href="{{ route('post.edit', [$post->id]) }}" class="btn btn-dark"><i class="bi bi-pencil"></i> Edit blog</a>
+      </div>
+      @endauth --}}
+    </div>
+    <div class="row gx-4 gx-lg-5 justify-content-center">
+      <div class="col-md-10 col-lg-8 col-xl-12">
+        <p>{{$post->content}}</p>
+        <h2 class="section-heading"></h2>
+        <span class="caption text-muted"></span>
+      </div>
+    </div>
   </div>
 </article>
 @endsection
