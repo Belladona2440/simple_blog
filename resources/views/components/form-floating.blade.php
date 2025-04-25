6439bf3592d05bb332aff9755c5fe86223b06aa4
Belladona2440 <belladonainblue@gmail.com>
2025-04-25 22:53:43 +0800

    add old input value to form field

--- resources/views/components/form-floating.blade.php
+++ resources/views/components/form-floating.blade.php
@@ -1,6 +1,6 @@
 @props(['type' => 'text', 'name', 'id' => $name, 'placeholder' => '', 'value', 'label'])
 
 <div class="form-floating border border-dark rounded mb-3">
-  <input type="{{ $type }}" class="form-control @error('{{ $name }}') is-invalid @enderror" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}" value="{{ old($name) }}" required>
+  <input type="{{ $type }}" class="form-control @error('{{ $name }}') is-invalid @enderror" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}" value="{{ old($name, $value ?? '') }}">
   <label for="{{ $id }}">{{ $label }}</label>
 </div>
