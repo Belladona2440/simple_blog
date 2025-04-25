8942a4e8c7435d1cec03d8bbabefa7f5b68f1c92
Belladona2440 <belladonainblue@gmail.com>
2025-04-25 23:35:28 +0800

    add posts-header and rename header to page-header

---
+++ resources/views/partials/posts-header.blade.php
@@ -0,0 +1,16 @@
+<div class="page-title position-relative">
+  <div class="container d-flex justify-content-between align-items-center">
+    <div class="d-flex align-items-center gap-2">
+      <a href="{{ route('post.create') }}" class="btn btn-dark d-flex align-items-center gap-2">
+        <i class="bi bi-pencil"></i>
+        <span>Write a Blog</span>
+      </a>
+    </div>      
+    <form action="" class="d-flex align-items-center gap-2">
+      <input type="text" class="form-control" placeholder="Search">
+      <button type="submit" class="btn btn-dark" title="Search">
+        <i class="bi bi-search"></i>
+      </button>
+    </form>
+  </div>
+</div>
