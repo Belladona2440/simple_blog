2204832c45fb7befa0d3c1964486a1f4c750600b
Belladona2440 <belladonainblue@gmail.com>
2025-04-26 21:02:27 +0800

    ommitted some code

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,23 +2,3 @@ import './bootstrap';
 
 import '../scss/app.scss';
 import * as bootstrap from 'bootstrap';
-import Swal from 'sweetalert2';
-//window.Swal = Swal; //use globally
-
-if (window.LogoutSuccess) {
-  const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.onmouseenter = Swal.stopTimer;
-      toast.onmouseleave = Swal.resumeTimer;
-    }
-  });
-  Toast.fire({
-    icon: "success",
-    title: "Signed in successfully"
-  });
-} 
