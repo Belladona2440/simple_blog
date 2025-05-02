90e3ed83a9ab1cba7f63e5b8e787b2a0cc322067
Belladona2440 <belladonainblue@gmail.com>
2025-05-02 20:34:38 +0800

    create folder auth for forgot password

---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+  public function showResetForm($token) {
+    return view('auth.reset-password', ['token' => $token]);
+  }
+
+  public function reset() {
+    
+  }
+}
