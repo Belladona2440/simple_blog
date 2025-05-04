a98107d6c99b58a12c6a5b5d7ca1118b0bc8e3f9
Belladona2440 <belladonainblue@gmail.com>
2025-05-04 18:09:20 +0800

    add flash message for sucess password reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
@@ -11,7 +13,18 @@ class ResetPasswordController extends Controller
     return view('auth.reset-password', ['token' => $token]);
   }
 
-  public function reset() {
-    
+  public function reset(Request $request) {
+    $request->validate([
+      'token' => 'required',
+      'email' => 'required|email',
+      'password' => 'required|confirmed|min:6'
+    ]);
+
+    $status = Password::reset($request->only('email', 'password', 'password_confirmation', 'token'), 
+      function($user, $password) {
+        $user->forceFill(['password' => Hash::make($password)])->save();
+      });
+
+    return $status == Password::PASSWORD_RESET ? redirect()->route('login')->with('success', 'Password reset successful. You may now login.') : back()->withErrors(['email' => [__($status)]]);
   }
 }
