4f33e775f8d8cb887c59a8311a6be837f53a63fa
Belladona2440 <belladonainblue@gmail.com>
2025-05-04 18:42:21 +0800

    rename success-message component

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,3 +1,17 @@
-@if (session()->has('message'))
-  <h2>{{ session('message')}}</h2>
+@props(['type' => 'success'])
+
+@php
+  $alertType = match ($type) {
+    'error' => 'danger',
+    'warning' => 'warning',
+    'info' => 'info',
+    default => $type
+  }
+@endphp
+
+@if (session($type))
+  <div class="alert alert-{{ $alertType }} alert-dismissible fade show small" role="alert">
+    {{ session($type) }}
+    <button type="button" class="btn-close btn-close-sm" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
 @endif
