40df05a8642122832ddce71e8cf5781c3a693c01
Belladona2440 <belladonainblue@gmail.com>
2025-05-04 18:15:04 +0800

    nothing changed

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -15,8 +15,13 @@
           <div class="card-body p-3 p-md-4 p-xl-5">
             <form action="{{ route('password.email') }}" method="POST">
               @csrf
+              <div class="alert alert-success alert-dismissible fade show small" role="alert">
+                  Password reset successful. You may now login.
+                <button type="button" class="btn-close btn-close-xs small" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+              <x-success-message type="error"></x-success-message>
               <div class="row gy-3 overflow-hidden">
-                <h4 class="text-center mb-4">Reset Password</h4>
+                <h4 class="text-center mb-4">Forgot Password</h4>
                 <x-form-field>
                   <x-form-floating type="email" name="email" id="email" placeholder="choi@kupal.com" label="Email" value="{{ old('email') }}" />
                   <x-form-error name="email" />
