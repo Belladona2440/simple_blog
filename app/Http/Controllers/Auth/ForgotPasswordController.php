006148b84eae1cd275d6cdf797275352dca74bd5
Belladona2440 <belladonainblue@gmail.com>
2025-05-06 00:34:51 +0800

    update send reset link method

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\PasswordResetEmail;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Post;
+use App\Models\User;
 
 class ForgotPasswordController extends Controller
 {
@@ -14,13 +18,33 @@ class ForgotPasswordController extends Controller
   }
 
   public function sendResetLink(Request $request) {
-    $request->validate(['email' => 'required|email']);
+    /* $request->validate(['email' => 'required|email']);
 
     $status = Password::sendResetLink(
       $request->only('email')
     );
 
-    return $status === Password::RESET_LINK_SENT ? back()->with(['status' => __($status)]) : back()->withErrors(['email' => __($status)]);
+    if ($status === Password::RESET_LINK_SENT) {
+      return redirect()->route('login')->with('success', 'A reset link has been sent to your email');
+    } else {
+      return back()->withErrors(['email' => __($status)]);
+    } */
+    //return $status === Password::RESET_LINK_SENT ? back()->with(['status' => __($status)]) : back()->withErrors(['email' => __($status)]);
 
+    //return redirect()->route('login')->with('error', 'A reset link has been sent to your email');
+
+    $request->validate(['email' => 'required|email']);
+
+    $user = \App\Models\User::where('email', $request->email)->first();
+
+    if (!$user) {
+        return back()->withErrors(['email' => 'We can\'t find a user with that email address.']);
+    }
+
+    $token = Password::createToken($user);
+
+    Mail::to($user->email)->queue(new \App\Mail\PasswordResetEmail($user, $token));
+
+    return redirect()->route('login')->with('success', 'A reset link has been sent to your email.');
   }
 }
